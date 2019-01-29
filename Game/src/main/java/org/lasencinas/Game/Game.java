f292040e94bc6a0c3aa6f6466695ac052f599bd5
ManuelCobos <manuelcobosibanez@gmail.com>
2019-01-29 17:32:20 +0100

    comienzo proyecto BowlingGame

---
+++ Game/src/main/java/org/lasencinas/Game/Game.java
@@ -0,0 +1,5 @@
+package org.lasencinas.Game;
+
+public class Game {
+	
+}
---
+++ Game/src/test/java/org/lasencinas/Game/GameTest.java
@@ -0,0 +1,13 @@
+package org.lasencinas.Game;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GameTest{
+  @Test
+   public void canMakeGame() {
+	   new Game();
+   }
+}
